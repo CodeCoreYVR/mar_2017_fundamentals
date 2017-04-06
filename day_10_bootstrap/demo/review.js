0399270ff888853ebf730f345e7f1565b8814986
Jacob Tran <jacobtran@gmail.com>
2017-04-05 21:08:20 -0700

    add day 10 files

---
+++ day_10_bootstrap/demo/review.js
@@ -0,0 +1,66 @@
+// good evening everyone!!
+
+// jquery
+// html
+// include the jquery library
+<html>
+
+<form>
+  <input id="first-name" type="text" />
+  <input id="submit" type="submit" />
+</form>
+
+$('#first-name');
+$('#submit');
+$('form').children();
+$('form').firstChild();
+$('input[type=text]');
+
+$('input[type=submit]').submit(function(event){
+  event.preventDefault();
+});
+
+// firebase - ???
+
+
+
+
+
+// front-end frameworks: bootstrap
+// cssgrid
+// +jquery
+
+
+
+
+
+
+
+
+
+// chat widget
+// ask them for their name first (prompt)
+
+// text field
+// submit
+
+// text area
+//
+// Jacob: Hello
+// Jacob: How are you?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
